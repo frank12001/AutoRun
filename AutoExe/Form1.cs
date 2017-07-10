130b14dfc9adcf72fb3c8b9344e1419331daa916
FanTang <gihhgihh11@gmail.com>
2017-07-10 18:00:38 +0800

    Version 1

---
+++ AutoExe/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoExe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < int.Parse(textBox2.Text); i++)
+                {
+                    System.Diagnostics.Process.Start(System.Windows.Forms.Application.StartupPath + @"\" + textBox3.Text + @".exe");
+                    Thread.Sleep(int.Parse(textBox1.Text) * 1000);
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+    }
+}
